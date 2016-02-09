50b7b6ae777d7e222a2c64933a498d24486f4fc0
Kristy Ramage <kristy.raamage@gmail.com>
2016-02-09 13:38:14 +1300

    Added home page view

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,10 @@
+@extends('master')
+
+@section('title', 'Home')
+@section('meta-description', 'Just like twitter')
+	
+@section('content')
+
+    <div class="title">Twitter Clone</div>
+
+@endsection

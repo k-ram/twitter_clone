605044214b038905f177cccb7e22390d304922bf
Kristy Ramage <kristy.raamage@gmail.com>
2016-02-09 13:14:14 +1300

    Add columns to the  users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('username', 20)->unique();
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->string('profileImage', 30);
+            $table->string('description', 150);
+            $table->string('website', 100);
             $table->rememberToken();
+            $table->string('background', 30);
             $table->timestamps();
         });
     }

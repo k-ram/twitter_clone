4df6e0b6e767331b498f8f59eb131d4229508fb3
Kristy Ramage <kristy.raamage@gmail.com>
2016-02-12 13:50:26 +1300

    created tag table and model

---
+++ database/migrations/2016_02_12_004502_create_tags_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 40);
+
+            // if you have timestamps you have to counter it -- research
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tags');
+    }
+}

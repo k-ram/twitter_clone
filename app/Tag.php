d0faad2f12d1d717ec704fe85f8312ca3d92c210
Kristy Ramage <kristy.raamage@gmail.com>
2016-02-12 14:33:35 +1300

    created relstionships between tables

--- app/Tag.php
+++ app/Tag.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    public function tweets(){
+    	return $this->belongsToMany('App\Tweet');
+    }
 }
 
 // php artisan make:model Tag <- how model is created in the commandline
